aba2e4fa3e002ade4c45d16b14c9018c622e699f
Park HaYeon <he_yan@naver.com>
2022-10-11 23:38:19 +0900

    third commit

---
+++ script.js
@@ -0,0 +1,101 @@
+/*gnb dropdown toggle*/
+function showham1() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu1").style.top = "80px";
+  document.getElementById("closehambtn1").style.top = "325px";
+}
+
+function showham2() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu2").style.top = "80px";
+  document.getElementById("closehambtn2").style.top = "325px";
+}
+
+function showham3() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu3").style.top = "80px";
+  document.getElementById("closehambtn3").style.top = "325px";
+}
+
+function showham4() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu4").style.top = "80px";
+  document.getElementById("closehambtn4").style.top = "325px";
+}
+
+function showham5() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu5").style.top = "80px";
+  document.getElementById("closehambtn5").style.top = "325px";
+}
+
+function showham6() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu6").style.top = "80px";
+  document.getElementById("closehambtn6").style.top = "325px";
+}
+
+function showham7() {
+  document.getElementById("filter-dark").style.display = "block";
+  document.getElementById("sd-menu7").style.top = "80px";
+  document.getElementById("closehambtn7").style.top = "325px";
+}
+//for (i = 1; i <= 7; i++)
+//    (function (i) {
+//        var btn = document.getElementById('sd-menu' + i);
+//        btn.onclick = function () {
+//            document.getElementById('filter-dark').style.display = "none";
+//            document.getElementById('sd-menu' + i).style.top = "-350px";
+//            document.getElementById('closehambtn').style.top = "-50px";
+//        }
+//    })(i);
+function closeham1() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu1").style.top = "-350px";
+  document.getElementById("closehambtn1").style.top = "-50px";
+}
+
+function closeham2() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu2").style.top = "-350px";
+  document.getElementById("closehambtn2").style.top = "-50px";
+}
+
+function closeham3() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu3").style.top = "-350px";
+  document.getElementById("closehambtn3").style.top = "-50px";
+}
+
+function closeham4() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu4").style.top = "-350px";
+  document.getElementById("closehambtn4").style.top = "-50px";
+}
+
+function closeham5() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu5").style.top = "-350px";
+  document.getElementById("closehambtn5").style.top = "-50px";
+}
+
+function closeham6() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu6").style.top = "-350px";
+  document.getElementById("closehambtn6").style.top = "-50px";
+}
+
+function closeham7() {
+  document.getElementById("filter-dark").style.display = "none";
+  document.getElementById("sd-menu7").style.top = "-350px";
+  document.getElementById("closehambtn7").style.top = "-50px";
+}
+
+/*여기부터 whole menu*/
+function openPcTap() {
+  document.getElementById("pcTap").style.display = "block";
+}
+
+function closePcTap() {
+  document.getElementById("pcTap").style.display = "none";
+}
