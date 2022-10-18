9af258f69b0200a173e0014c2101fd519785771f
Park HaYeon <he_yan@naver.com>
2022-10-19 00:40:19 +0900

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
 /*gnb dropdown toggle*/
 function showham1() {
   document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu1").style.top = "80px";
-  document.getElementById("closehambtn1").style.top = "325px";
+  document.getElementById("sd-menu1").style.top = "100px";
+  document.getElementById("closehambtn1").style.top = "125px";
 }
 
 function showham2() {
   document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu2").style.top = "80px";
-  document.getElementById("closehambtn2").style.top = "325px";
+  document.getElementById("sd-menu2").style.top = "100px";
+  document.getElementById("closehambtn2").style.top = "125px";
 }
 
 function showham3() {
   document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu3").style.top = "80px";
-  document.getElementById("closehambtn3").style.top = "325px";
+  document.getElementById("sd-menu3").style.top = "100px";
+  document.getElementById("closehambtn3").style.top = "125px";
 }
 
 function showham4() {
   document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu4").style.top = "80px";
-  document.getElementById("closehambtn4").style.top = "325px";
+  document.getElementById("sd-menu4").style.top = "100px";
+  document.getElementById("closehambtn4").style.top = "125px";
 }
 
-function showham5() {
-  document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu5").style.top = "80px";
-  document.getElementById("closehambtn5").style.top = "325px";
-}
 
-function showham6() {
-  document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu6").style.top = "80px";
-  document.getElementById("closehambtn6").style.top = "325px";
-}
-
-function showham7() {
-  document.getElementById("filter-dark").style.display = "block";
-  document.getElementById("sd-menu7").style.top = "80px";
-  document.getElementById("closehambtn7").style.top = "325px";
-}
-//for (i = 1; i <= 7; i++)
-//    (function (i) {
-//        var btn = document.getElementById('sd-menu' + i);
-//        btn.onclick = function () {
-//            document.getElementById('filter-dark').style.display = "none";
-//            document.getElementById('sd-menu' + i).style.top = "-350px";
-//            document.getElementById('closehambtn').style.top = "-50px";
-//        }
-//    })(i);
 function closeham1() {
   document.getElementById("filter-dark").style.display = "none";
   document.getElementById("sd-menu1").style.top = "-350px";
@@ -72,25 +47,7 @@ function closeham4() {
   document.getElementById("sd-menu4").style.top = "-350px";
   document.getElementById("closehambtn4").style.top = "-50px";
 }
-
-function closeham5() {
-  document.getElementById("filter-dark").style.display = "none";
-  document.getElementById("sd-menu5").style.top = "-350px";
-  document.getElementById("closehambtn5").style.top = "-50px";
-}
-
-function closeham6() {
-  document.getElementById("filter-dark").style.display = "none";
-  document.getElementById("sd-menu6").style.top = "-350px";
-  document.getElementById("closehambtn6").style.top = "-50px";
-}
-
-function closeham7() {
-  document.getElementById("filter-dark").style.display = "none";
-  document.getElementById("sd-menu7").style.top = "-350px";
-  document.getElementById("closehambtn7").style.top = "-50px";
-}
-
+//ham
 /*여기부터 whole menu*/
 function openPcTap() {
   document.getElementById("pcTap").style.display = "block";
@@ -99,3 +56,42 @@ function openPcTap() {
 function closePcTap() {
   document.getElementById("pcTap").style.display = "none";
 }
+
+//video background
+jQuery( function() {
+  jQuery( '#background' ).YTPlayer();
+});
+
+/*Navbar changes color on scrolling*/
+//중복이 안 됨
+window.onscroll = () => {
+  const nav = document.querySelector('.navbar');//원래 #navbar였음
+  if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+};
+
+
+//expanding flex cards
+const options = document.querySelectorAll('.option');
+
+options.forEach(element => {
+  element.addEventListener('click', event => {
+    options.forEach(el => el.classList.remove('active'));
+    event.target.closest('.option').classList.add('active');
+  })
+});
+
+
+//top-fix
+$( document ).ready( function() {
+  $( window ).scroll( function() {
+    if ( $( this ).scrollTop() > 200 ) {
+      $( '.top' ).fadeIn();
+    } else {
+      $( '.top' ).fadeOut();
+    }
+  });
+  $( '.top' ).click( function() {
+    $( 'html, body' ).animate( { scrollTop : 0 }, 1000 );
+    return false;
+  });
+});
